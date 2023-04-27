75b0ef18842b6e647b0dc0abd332ffaab25f2d59
EnkhtuvshinT <128656040+EnkhtuvshinT@users.noreply.github.com>
2023-04-27 15:45:41 +0900

    Add files via upload

--- 202388004/test3.c
+++ 202388004/test3.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int x=10, y=10;
-
-    printf("x=%d\n",x);
-    printf("++x의 값=%d\n",++x);
-    printf("x=%d\n\n",x);
-
-    printf("y=%d\n",y);
-    printf("y++의 값=%d\n",y++);
-    printf("y=%d\n",y);
-
-    return 0;
+double rate;
+double usd;
+int krw;
+printf("환율을 입력하시오: ");
+scanf("%lf", &rate);
+printf("원화 금액을 입력하시오: ");
+scanf("%d", &krw);
+usd = krw / rate;
+printf("원화 %d원은 %lf달러입니다.\n", krw, usd);
+return 0;
 }
